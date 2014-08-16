8df23b6214e55eaaa85d37652f9201ad3f34ad82
ffMathy <mathias.lorenzen@live.com>
2014-08-16 11:50:31 +0200

    added enums.

--- PInvoke/PInvoke/NativeMethods.cs
+++ PInvoke/PInvoke/NativeMethods.cs
@@ -6,19 +6,90 @@ namespace PInvoke
     public static class NativeMethods
     {
         [DllImport("user32.dll")]
-        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        public static extern int SetWindowLong(IntPtr hWnd, WindowLongFlags nIndex, SetWindowLongFlags dwNewLong);
 
         [DllImport("user32.dll", SetLastError = true)]
         public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
 
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, SetWindowPosFlags uFlags);
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy,
+            SetWindowPosFlags uFlags);
 
         [DllImport("gdi32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool DeleteObject(IntPtr hObject);
 
         [Flags]
+        public enum WindowLongFlags : int
+        {
+            GWL_EXSTYLE = -20,
+            GWLP_HINSTANCE = -6,
+            GWLP_HWNDPARENT = -8,
+            GWL_ID = -12,
+            GWL_STYLE = -16,
+            GWL_USERDATA = -21,
+            GWL_WNDPROC = -4,
+            DWLP_USER = 0x8,
+            DWLP_MSGRESULT = 0x0,
+            DWLP_DLGPROC = 0x4
+        }
+
+        [Flags]
+        public enum SetWindowLongFlags : uint
+        { 
+            WS_OVERLAPPED = 0,
+            WS_POPUP = 0x80000000,
+            WS_CHILD = 0x40000000,
+            WS_MINIMIZE = 0x20000000,
+            WS_VISIBLE = 0x10000000,
+            WS_DISABLED = 0x8000000,
+            WS_CLIPSIBLINGS = 0x4000000,
+            WS_CLIPCHILDREN = 0x2000000,
+            WS_MAXIMIZE = 0x1000000,
+            WS_CAPTION = 0xC00000,
+            WS_BORDER = 0x800000,
+            WS_DLGFRAME = 0x400000,
+            WS_VSCROLL = 0x200000,
+            WS_HSCROLL = 0x100000,
+            WS_SYSMENU = 0x80000,
+            WS_THICKFRAME = 0x40000,
+            WS_GROUP = 0x20000,
+            WS_TABSTOP = 0x10000,
+            WS_MINIMIZEBOX = 0x20000,
+            WS_MAXIMIZEBOX = 0x10000,
+            WS_TILED = WS_OVERLAPPED,
+            WS_ICONIC = WS_MINIMIZE,
+            WS_SIZEBOX = WS_THICKFRAME,
+
+            WS_EX_DLGMODALFRAME = 0x0001,
+            WS_EX_NOPARENTNOTIFY = 0x0004,
+            WS_EX_TOPMOST = 0x0008,
+            WS_EX_ACCEPTFILES = 0x0010,
+            WS_EX_TRANSPARENT = 0x0020,
+            WS_EX_MDICHILD = 0x0040,
+            WS_EX_TOOLWINDOW = 0x0080,
+            WS_EX_WINDOWEDGE = 0x0100,
+            WS_EX_CLIENTEDGE = 0x0200,
+            WS_EX_CONTEXTHELP = 0x0400,
+            WS_EX_RIGHT = 0x1000,
+            WS_EX_LEFT = 0x0000,
+            WS_EX_RTLREADING = 0x2000,
+            WS_EX_LTRREADING = 0x0000,
+            WS_EX_LEFTSCROLLBAR = 0x4000,
+            WS_EX_RIGHTSCROLLBAR = 0x0000,
+            WS_EX_CONTROLPARENT = 0x10000,
+            WS_EX_STATICEDGE = 0x20000,
+            WS_EX_APPWINDOW = 0x40000,
+            WS_EX_OVERLAPPEDWINDOW = (WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE),
+            WS_EX_PALETTEWINDOW = (WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST),
+            WS_EX_LAYERED = 0x00080000,
+            WS_EX_NOINHERITLAYOUT = 0x00100000, 
+            WS_EX_LAYOUTRTL = 0x00400000,
+            WS_EX_COMPOSITED = 0x02000000,
+            WS_EX_NOACTIVATE = 0x08000000,
+    }
+
+    [Flags]
         public enum SetWindowPosFlags : uint
         {
             // ReSharper disable InconsistentNaming
