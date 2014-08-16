6c54d09b231e66bff0744eeef8b129905ca2d85a
ffMathy <mathias.lorenzen@live.com>
2014-08-16 11:25:49 +0200

    added GetWindowLong.

--- PInvoke/PInvoke/NativeMethods.cs
+++ PInvoke/PInvoke/NativeMethods.cs
@@ -8,6 +8,9 @@ namespace PInvoke
         [DllImport("user32.dll")]
         public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
         [DllImport("gdi32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool DeleteObject(IntPtr hObject);
