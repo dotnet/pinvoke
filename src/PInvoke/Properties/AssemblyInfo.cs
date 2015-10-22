775a699eb0465ff2f5cce9de8dec56aa6b3cfdf7
Andrew Arnott <andarno@microsoft.com>
2015-10-22 09:50:30 -0700

    Apply versioning policy

--- src/PInvoke/Properties/AssemblyInfo.cs
+++ src/PInvoke/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("154e6929-278e-43d2-b955-f99b49d2e3f9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
