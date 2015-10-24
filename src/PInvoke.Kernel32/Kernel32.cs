5097db192f9c34e1a75e1d98e3fd1908ababe437
Andrew Arnott <andarno@microsoft.com>
2015-10-23 18:35:57 -0700

    Add more data and docs for finding files

--- src/PInvoke.Kernel32/Kernel32.cs
+++ src/PInvoke.Kernel32/Kernel32.cs
@@ -17,6 +17,90 @@ namespace PInvoke
         /// </summary>
         public static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
 
+        [Flags]
+        public enum FileAttribute
+        {
+            /// <summary>
+            /// A file or directory that is an archive file or directory. Applications typically use this attribute to mark files for backup or removal.
+            /// </summary>
+            Archive = 0x20,
+
+            /// <summary>
+            /// A file or directory that is compressed. For a file, all of the data in the file is compressed. For a directory, compression is the default for newly created files and subdirectories.
+            /// </summary>
+            Compressed = 0x800,
+
+            /// <summary>
+            /// This value is reserved for system use.
+            /// </summary>
+            Device = 0x40,
+
+            /// <summary>
+            /// The handle that identifies a directory.
+            /// </summary>
+            Directory = 0x10,
+
+            /// <summary>
+            /// A file or directory that is encrypted. For a file, all data streams in the file are encrypted. For a directory, encryption is the default for newly created files and subdirectories.
+            /// </summary>
+            Encrypted = 0x4000,
+
+            /// <summary>
+            /// The file or directory is hidden. It is not included in an ordinary directory listing.
+            /// </summary>
+            Hidden = 0x2,
+
+            /// <summary>
+            /// The directory or user data stream is configured with integrity (only supported on ReFS volumes). It is not included in an ordinary directory listing. The integrity setting persists with the file if it's renamed. If a file is copied the destination file will have integrity set if either the source file or destination directory have integrity set.
+            /// </summary>
+            IntegrityStream = 0x8000,
+
+            /// <summary>
+            /// A file that does not have other attributes set. This attribute is valid only when used alone.
+            /// </summary>
+            Normal = 0x80,
+
+            /// <summary>
+            /// The file or directory is not to be indexed by the content indexing service.
+            /// </summary>
+            NotContentIndexed = 0x2000,
+
+            /// <summary>
+            /// The user data stream not to be read by the background data integrity scanner (AKA scrubber). When set on a directory it only provides inheritance. This flag is only supported on Storage Spaces and ReFS volumes. It is not included in an ordinary directory listing.
+            /// </summary>
+            NoScrubData = 0x20000,
+
+            /// <summary>
+            /// The data of a file is not available immediately. This attribute indicates that the file data is physically moved to offline storage. This attribute is used by Remote Storage, which is the hierarchical storage management software. Applications should not arbitrarily change this attribute.
+            /// </summary>
+            Offline = 0x1000,
+
+            /// <summary>
+            /// A file that is read-only. Applications can read the file, but cannot write to it or delete it. This attribute is not honored on directories.
+            /// </summary>
+            ReadOnly = 0x1,
+
+            /// <summary>
+            /// A file or directory that has an associated reparse point, or a file that is a symbolic link.
+            /// </summary>
+            ReparsePoint = 0x400,
+
+            /// <summary>
+            /// A file or directory that the operating system uses a part of, or uses exclusively.
+            /// </summary>
+            System = 0x4,
+
+            /// <summary>
+            /// A file that is being used for temporary storage. File systems avoid writing data back to mass storage if sufficient cache memory is available, because typically, an application deletes a temporary file after the handle is closed. In that scenario, the system can entirely avoid writing the data. Otherwise, the data is written after the handle is closed.
+            /// </summary>
+            Temporary = 0x100,
+
+            /// <summary>
+            /// This value is reserved for system use.
+            /// </summary>
+            Virtual = 0x10000,
+        }
+
         /// <summary>
         /// Closes an open object handle.
         /// </summary>
@@ -75,16 +159,66 @@ namespace PInvoke
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
         public struct WIN32_FIND_DATA
         {
-            public uint dwFileAttributes;
+            /// <summary>
+            /// The file attributes of a file.
+            /// </summary>
+            public FileAttribute dwFileAttributes;
+
+            /// <summary>
+            /// A FILETIME structure that specifies when a file or directory was created.
+            /// If the underlying file system does not support creation time, this member is zero.
+            /// </summary>
             public FILETIME ftCreationTime;
+
+            /// <summary>
+            /// A FILETIME structure.
+            /// For a file, the structure specifies when the file was last read from, written to, or for executable files, run.
+            /// For a directory, the structure specifies when the directory is created.If the underlying file system does not support last access time, this member is zero.
+            /// On the FAT file system, the specified date for both files and directories is correct, but the time of day is always set to midnight.
+            /// </summary>
             public FILETIME ftLastAccessTime;
+
+            /// <summary>
+            /// A FILETIME structure.
+            /// For a file, the structure specifies when the file was last written to, truncated, or overwritten, for example, when WriteFile or SetEndOfFile are used.The date and time are not updated when file attributes or security descriptors are changed.
+            /// For a directory, the structure specifies when the directory is created.If the underlying file system does not support last write time, this member is zero.
+            /// </summary>
             public FILETIME ftLastWriteTime;
+
+            /// <summary>
+            /// The high-order DWORD value of the file size, in bytes.
+            /// This value is zero unless the file size is greater than MAXDWORD.
+            /// The size of the file is equal to(nFileSizeHigh* (MAXDWORD+1)) + nFileSizeLow.
+            /// </summary>
             public uint nFileSizeHigh;
+
+            /// <summary>
+            /// The low-order DWORD value of the file size, in bytes.
+            /// </summary>
             public uint nFileSizeLow;
+
+            /// <summary>
+            /// If the dwFileAttributes member includes the FILE_ATTRIBUTE_REPARSE_POINT attribute, this member specifies the reparse point tag.
+            /// Otherwise, this value is undefined and should not be used.
+            /// For more information see Reparse Point Tags.
+            /// </summary>
             public uint dwReserved0;
+
+            /// <summary>
+            /// Reserved for future use.
+            /// </summary>
             public uint dwReserved1;
+
+            /// <summary>
+            /// The name of the file.
+            /// </summary>
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
             public string cFileName;
+
+            /// <summary>
+            /// An alternative name for the file.
+            /// This name is in the classic 8.3 file name format.
+            /// </summary>
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 14)]
             public string cAlternateFileName;
         }
