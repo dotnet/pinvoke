0d75975262d5394dbc22fb7e38e431770bf56252
Andrew Arnott <andarno@microsoft.com>
2015-10-24 09:28:48 -0700

    Add missing file header

--- src/Kernel32.Desktop/Properties/AssemblyInfo.cs
+++ src/Kernel32.Desktop/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+using System.Reflection;
 
 [assembly: AssemblyTitle("PInvoke.Kernel32")]
 [assembly: AssemblyProduct("PInvoke.Kernel32")]
--- templates/LIBNAME.Desktop/Properties/AssemblyInfo.cs
+++ templates/LIBNAME.Desktop/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
