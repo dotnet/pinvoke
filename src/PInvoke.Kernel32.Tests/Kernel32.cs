4488abbfbce1e840c36d507766997737a9db602c
Andrew Arnott <andarno@microsoft.com>
2015-10-23 20:24:13 -0700

    Add FindNextFile test

--- src/PInvoke.Kernel32.Tests/Kernel32.cs
+++ src/PInvoke.Kernel32.Tests/Kernel32.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 
 using System;
+using System.IO;
 using PInvoke;
 using Xunit;
 using static PInvoke.Kernel32;
@@ -9,12 +10,47 @@ using static PInvoke.Kernel32;
 public class Kernel32
 {
     [Fact]
-    public void FindFiles()
+    public void FindFirstFile_NoMatches()
     {
         WIN32_FIND_DATA data;
         using (var handle = FindFirstFile("foodoesnotexist", out data))
         {
             Assert.True(handle.IsInvalid);
+        }
+    }
+
+    [Fact]
+    public void FindFirstFile_FindNextFile()
+    {
+        string testPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        try
+        {
+            Directory.CreateDirectory(testPath);
+            string aTxt = Path.Combine(testPath, "a.txt");
+            File.WriteAllText(aTxt, string.Empty);
+            File.SetAttributes(aTxt, FileAttributes.Archive);
+
+            var bTxt = Path.Combine(testPath, "b.txt");
+            File.WriteAllText(bTxt, string.Empty);
+            File.SetAttributes(bTxt, FileAttributes.Normal);
+
+            WIN32_FIND_DATA data;
+            using (var handle = FindFirstFile(Path.Combine(testPath, "*.txt"), out data))
+            {
+                Assert.False(handle.IsInvalid);
+                Assert.Equal("a.txt", data.cFileName);
+                Assert.Equal(FileAttribute.Archive, data.dwFileAttributes);
+
+                Assert.True(FindNextFile(handle, out data));
+                Assert.Equal("b.txt", data.cFileName);
+                Assert.Equal(FileAttribute.Normal, data.dwFileAttributes);
+
+                Assert.False(FindNextFile(handle, out data));
+            }
+        }
+        finally
+        {
+            Directory.Delete(testPath, true);
         }
     }
 }
