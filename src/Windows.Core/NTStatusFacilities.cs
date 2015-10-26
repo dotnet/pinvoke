acc6113b9afa32740dce9f764a88c149c1492636
Julien Roncaglia <julien.roncaglia@itg.com>
2015-10-26 18:11:21 +0100

    Add HIDP_ NTStatus members and al Win32 error codes

---
+++ src/Windows.Core/NTStatusFacilities.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace PInvoke
+{
+    /// <summary>
+    /// Represent the 16 facility bits in <see cref="NTStatus"/> enumeration.
+    /// </summary>
+    internal static class NTStatusFacilities
+    {
+        public const uint HidErrorCode = (uint)0x11 << 16;
+    }
+}
---
+++ src/Windows.Core/NTStatusServerity.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace PInvoke
+{
+    /// <summary>
+    /// Represent the 4 high bits of a <see cref="NTStatus"/> enumeration : Severity (2 bits), Customer code (1 bit) and Reserved (1 bit).
+    /// </summary>
+    internal static class NTStatusServerity
+    {
+        public const uint Success = (uint)0xC << 28;
+        public const uint Informational = (uint)0x4 << 28;
+        public const uint Warning = (uint)0x8 << 28;
+        public const uint Error = (uint)0xC << 28;
+    }
+}
