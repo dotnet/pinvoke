6227edd82c9457f70eafc64c92652454d2ebe2cf
Julien Roncaglia <julien.roncaglia@itg.com>
2015-10-27 09:10:28 +0100

    Run AddNewLibrary.ps1 for SetupApi.dll

---
+++ src/SetupApi.Shared/SetupApi.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace PInvoke
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// Exported functions from the SetupApi.dll Windows library
+    /// that are available to Desktop and Store apps.
+    /// </summary>
+    public static partial class SetupApi
+    {
+    }
+}
