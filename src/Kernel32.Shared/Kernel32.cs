d5a4742a278857ed2ff4921defff99b37829a174
Andrew Arnott <andarno@microsoft.com>
2015-10-31 08:54:35 -0700

    Add Kernel32's FormatMessage

--- src/Kernel32.Shared/Kernel32.cs
+++ src/Kernel32.Shared/Kernel32.cs
@@ -5,6 +5,7 @@ namespace PInvoke
 {
     using System;
     using System.Runtime.InteropServices;
+    using System.Text;
     using FILETIME = System.Runtime.InteropServices.ComTypes.FILETIME;
 
     /// <summary>
@@ -54,6 +55,61 @@ namespace PInvoke
         }
 
         /// <summary>
+        /// Flags passed to the <see cref="FormatMessage"/> method.
+        /// </summary>
+        [Flags]
+        public enum FormatMessageFlags
+        {
+            /// <summary>
+            /// The function allocates a buffer large enough to hold the formatted message, and places a pointer to the allocated buffer at the address specified by lpBuffer. The nSize parameter specifies the minimum number of TCHARs to allocate for an output message buffer. The caller should use the LocalFree function to free the buffer when it is no longer needed.
+            /// If the length of the formatted message exceeds 128K bytes, then FormatMessage will fail and a subsequent call to GetLastError will return ERROR_MORE_DATA.
+            /// In previous versions of Windows, this value was not available for use when compiling Windows Store apps. As of Windows 10 this value can be used.
+            /// Windows Server 2003 and Windows XP:
+            /// If the length of the formatted message exceeds 128K bytes, then FormatMessage will not automatically fail with an error of ERROR_MORE_DATA.
+            /// Windows 10:
+            /// LocalFree is not in the modern SDK, so it cannot be used to free the result buffer. Instead, use HeapFree (GetProcessHeap(), allocatedMessage). In this case, this is the same as calling LocalFree on memory.
+            /// Important: LocalAlloc() has different options: LMEM_FIXED, and LMEM_MOVABLE. FormatMessage() uses LMEM_FIXED, so HeapFree can be used. If LMEM_MOVABLE is used, HeapFree cannot be used.
+            /// </summary>
+            AllocateBuffer = 0x100,
+
+            /// <summary>
+            /// The Arguments parameter is not a va_list structure, but is a pointer to an array of values that represent the arguments.
+            /// This flag cannot be used with 64-bit integer values. If you are using a 64-bit integer, you must use the va_list structure.
+            /// </summary>
+            ArgumentArray = 0x2000,
+
+            /// <summary>
+            /// The lpSource parameter is a module handle containing the message-table resource(s) to search. If this lpSource handle is NULL, the current process's application image file will be searched. This flag cannot be used with <see cref="FromString"/>.
+            /// If the module has no message table resource, the function fails with ERROR_RESOURCE_TYPE_NOT_FOUND.
+            /// </summary>
+            FromHModule = 0x800,
+
+            /// <summary>
+            /// The lpSource parameter is a pointer to a null-terminated string that contains a message definition. The message definition may contain insert sequences, just as the message text in a message table resource may. This flag cannot be used with <see cref="FromHModule"/> or <see cref="FromSystem"/>.
+            /// </summary>
+            FromString = 0x400,
+
+            /// <summary>
+            /// The function should search the system message-table resource(s) for the requested message. If this flag is specified with <see cref="FromHModule"/>, the function searches the system message table if the message is not found in the module specified by lpSource. This flag cannot be used with <see cref="FromString"/>.
+            /// If this flag is specified, an application can pass the result of the GetLastError function to retrieve the message text for a system-defined error.
+            /// </summary>
+            FromSystem = 0x1000,
+
+            /// <summary>
+            /// Insert sequences in the message definition are to be ignored and passed through to the output buffer unchanged. This flag is useful for fetching a message for later formatting. If this flag is set, the Arguments parameter is ignored.
+            /// </summary>
+            IgnoreInserts = 0x200,
+
+            /// <summary>
+            /// The function ignores regular line breaks in the message definition text. The function stores hard-coded line breaks in the message definition text into the output buffer. The function generates no new line breaks.
+            /// Without this flag set:
+            /// There are no output line width restrictions. The function stores line breaks that are in the message definition text into the output buffer.
+            /// It specifies the maximum number of characters in an output line. The function ignores regular line breaks in the message definition text. The function never splits a string delimited by white space across a line break. The function stores hard-coded line breaks in the message definition text into the output buffer. Hard-coded line breaks are coded with the %n escape sequence.
+            /// </summary>
+            MaxWidthMask = 0xff,
+        }
+
+        /// <summary>
         /// Searches a directory for a file or subdirectory with a name and attributes that match those specified.
         /// For the most basic version of this function, see FindFirstFile.
         /// To perform this operation as a transacted operation, use the FindFirstFileTransacted function.
@@ -87,6 +143,53 @@ namespace PInvoke
         /// </returns>
         [DllImport(nameof(Kernel32))]
         public static extern SafeFindFilesHandle FindFirstFileEx(string lpFileName, FINDEX_INFO_LEVELS fInfoLevelId, out WIN32_FIND_DATA lpFindFileData, FINDEX_SEARCH_OPS fSearchOp, IntPtr lpSearchFilter, FindFirstFileExFlags dwAdditionalFlags);
+
+        /// <summary>
+        /// Formats a message string. The function requires a message definition as input. The message definition can come from a buffer passed into the function. It can come from a message table resource in an already-loaded module. Or the caller can ask the function to search the system's message table resource(s) for the message definition. The function finds the message definition in a message table resource based on a message identifier and a language identifier. The function copies the formatted message text to an output buffer, processing any embedded insert sequences if requested.
+        /// </summary>
+        /// <param name="dwFlags">
+        /// The formatting options, and how to interpret the lpSource parameter. The low-order byte of dwFlags specifies how the function handles line breaks in the output buffer. The low-order byte can also specify the maximum width of a formatted output line.
+        /// </param>
+        /// <param name="lpSource">
+        /// The location of the message definition. The type of this parameter depends upon the settings in the <paramref name="dwFlags"/> parameter.
+        /// If <see cref="FormatMessageFlags.FromHModule"/>: A handle to the module that contains the message table to search.
+        /// If <see cref="FormatMessageFlags.FromString"/>: Pointer to a string that consists of unformatted message text. It will be scanned for inserts and formatted accordingly.
+        /// If neither of these flags is set in dwFlags, then lpSource is ignored.
+        /// </param>
+        /// <param name="dwMessageId">
+        /// The message identifier for the requested message. This parameter is ignored if dwFlags includes <see cref="FormatMessageFlags.FromString" />.
+        /// </param>
+        /// <param name="dwLanguageId">
+        /// The language identifier for the requested message. This parameter is ignored if dwFlags includes <see cref="FormatMessageFlags.FromString"/>.
+        /// If you pass a specific LANGID in this parameter, FormatMessage will return a message for that LANGID only.If the function cannot find a message for that LANGID, it sets Last-Error to ERROR_RESOURCE_LANG_NOT_FOUND.If you pass in zero, FormatMessage looks for a message for LANGIDs in the following order:
+        /// Language neutral
+        /// Thread LANGID, based on the thread's locale value
+        /// User default LANGID, based on the user's default locale value
+        /// System default LANGID, based on the system default locale value
+        /// US English
+        /// If FormatMessage does not locate a message for any of the preceding LANGIDs, it returns any language message string that is present.If that fails, it returns ERROR_RESOURCE_LANG_NOT_FOUND.
+        /// </param>
+        /// <param name="lpBuffer">
+        /// A pointer to a buffer that receives the null-terminated string that specifies the formatted message. If dwFlags includes <see cref="FormatMessageFlags.AllocateBuffer" />, the function allocates a buffer using the LocalAlloc function, and places the pointer to the buffer at the address specified in lpBuffer.
+        /// This buffer cannot be larger than 64K bytes.
+        /// </param>
+        /// <param name="nSize">
+        /// If the <see cref="FormatMessageFlags.AllocateBuffer"/> flag is not set, this parameter specifies the size of the output buffer, in TCHARs. If <see cref="FormatMessageFlags.AllocateBuffer"/> is set,
+        /// this parameter specifies the minimum number of TCHARs to allocate for an output buffer.
+        /// The output buffer cannot be larger than 64K bytes.
+        /// </param>
+        /// <param name="Arguments">
+        /// An array of values that are used as insert values in the formatted message. A %1 in the format string indicates the first value in the Arguments array; a %2 indicates the second argument; and so on.
+        /// The interpretation of each value depends on the formatting information associated with the insert in the message definition.The default is to treat each value as a pointer to a null-terminated string.
+        /// By default, the Arguments parameter is of type va_list*, which is a language- and implementation-specific data type for describing a variable number of arguments.The state of the va_list argument is undefined upon return from the function.To use the va_list again, destroy the variable argument list pointer using va_end and reinitialize it with va_start.
+        /// If you do not have a pointer of type va_list*, then specify the FORMAT_MESSAGE_ARGUMENT_ARRAY flag and pass a pointer to an array of DWORD_PTR values; those values are input to the message formatted as the insert values.Each insert must have a corresponding element in the array.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is the number of TCHARs stored in the output buffer, excluding the terminating null character.
+        /// If the function fails, the return value is zero.To get extended error information, call GetLastError.
+        /// </returns>
+        [DllImport(nameof(Kernel32), CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int FormatMessage(FormatMessageFlags dwFlags, IntPtr lpSource, uint dwMessageId, uint dwLanguageId, StringBuilder lpBuffer, int nSize, IntPtr[] Arguments);
 
         /// <summary>
         /// Closes a file search handle opened by the FindFirstFile, FindFirstFileEx, FindFirstFileNameW, FindFirstFileNameTransactedW, FindFirstFileTransacted, FindFirstStreamTransactedW, or FindFirstStreamW functions.
