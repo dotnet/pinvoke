6533c2471fdc32cc524a22b9d38ebc8f7d993334
jmelosegui <elosegui@gmail.com>
2015-11-11 08:46:35 -0500

    Changing CopyToOutputDirectory from Always to PreserveNewest and improving the CreateNativeTxtFiles.ps1 script

--- templates/CreateNativeTxtFiles.ps1
+++ templates/CreateNativeTxtFiles.ps1
@@ -1,25 +1,21 @@
 ﻿<#
 .SYNOPSIS
 Export all "export functions" from a native Win32 dll.
-.DESCRIPTION
-TODO
 .PARAMETER AssemblyPath
 The path, including the file name, of the Win32 dll from which the "export functions" name will be exported.
-.PARAMETER OutputPath
-The path where the result file (LIBNAME.exports.txt) will be created.
+.PARAMETER OutputDir
+The path to the directory where the result file (LIBNAME.exports.txt) will be created.
 #>
 Param(
     [Parameter(Mandatory=$true)]
     [string]$AssemblyPath,
     [Parameter()]
-    [string]$OutputPath='.'
+    [string]$OutputDir='.'
 )
-
 
 if (Test-Path $AssemblyPath) {
 
     $exportedMethods = New-Object 'System.Collections.Generic.Dictionary[string,string]'
-    $fullMethodList = New-Object 'System.Collections.Generic.List[string]'
 
     dumpbin /exports $AssemblyPath | 
     Where { $_ -match '\s+\d+\s+[A-Z0-9]+\s+[A-Z0-9\s]{8}\s{1}(?<MethodName>\w+)' } |
@@ -45,14 +41,14 @@ if (Test-Path $AssemblyPath) {
     }
 
     if ($exportedMethods.Count -gt 0) {
-        $OutputPath = Resolve-Path $OutputPath
+        $OutputDir = Resolve-Path $OutputDir
         $LibraryName = [System.IO.Path]::GetFileNameWithoutExtension($AssemblyPath);
-        $filePath = [System.IO.Path]::Combine($OutputPath, $LibraryName + '.exports.txt')
+        $filePath = [System.IO.Path]::Combine($OutputDir, $LibraryName + '.exports.txt')
         
         Set-Content $filePath ($exportedMethods.GetEnumerator() | % { $_.Key+$_.Value } | Sort-Object)
         Write-Output "Method names exported to $filePath"
-		Write-Output "Please add the $LibraryName.exports.txt to either the $LibraryName.Desktop or $LibraryName project as appropriate."
-		Write-Output "Make be sure this file gets copied as a build output."
+        Write-Output "Please add the $LibraryName.exports.txt to either the $LibraryName.Desktop or $LibraryName project as appropriate."
+        Write-Output "Make be sure this file gets copied as a build output."
     }
 }
 else{
