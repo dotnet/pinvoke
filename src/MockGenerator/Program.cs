37995f02e4513dbba0c3d45ebe3b3f81b7a9e92c
ffMathy <mathias.lorenzen@live.com>
2015-11-16 21:17:28 +0100

    new changes to generation.

--- src/MockGenerator/Program.cs
+++ src/MockGenerator/Program.cs
@@ -14,6 +14,9 @@ namespace MockGenerator
 
     class Program
     {
+        static readonly SyntaxTrivia WhitespaceCharacter = SyntaxFactory.Whitespace(" ");
+        static readonly SyntaxTrivia NewLineCharacter = SyntaxFactory.Whitespace("\n");
+
         static void Main(string[] args)
         {
             var currentDirectory = Environment.CurrentDirectory;
@@ -51,14 +54,14 @@ namespace MockGenerator
                         modifiers.Remove(staticKeyword);
                     }
 
-                    var interfaceName = SyntaxFactory
-                        .IdentifierName($"I{classDeclaration.Identifier.Text}Mockable")
-                        .WithLeadingTrivia(SyntaxFactory.Whitespace(" "))
-                        .WithTrailingTrivia(SyntaxFactory.Whitespace(" "));
+                    var interfaceIdentifier = SyntaxFactory.IdentifierName($"I{classDeclaration.Identifier.Text}Mockable");
 
                     var baseList = classDeclaration.BaseList ?? SyntaxFactory.BaseList();
                     baseList = baseList.AddTypes(
-                        SyntaxFactory.SimpleBaseType(interfaceName));
+                        SyntaxFactory.SimpleBaseType(
+                            interfaceIdentifier
+                                .WithLeadingTrivia(WhitespaceCharacter)
+                                .WithTrailingTrivia(WhitespaceCharacter)));
 
                     var newClassDeclaration = SyntaxFactory.ClassDeclaration(
                         classDeclaration.AttributeLists,
@@ -71,9 +74,131 @@ namespace MockGenerator
                     compilationUnit = compilationUnit.ReplaceNode(classDeclaration, newClassDeclaration);
                     classDeclaration = newClassDeclaration;
 
+                    var interfaceDeclaration = SyntaxFactory.InterfaceDeclaration(
+                        SyntaxFactory.List<AttributeListSyntax>(),
+                        SyntaxFactory.TokenList(
+                            SyntaxFactory
+                                .Token(SyntaxKind.PublicKeyword)
+                                .WithTrailingTrivia(WhitespaceCharacter)),
+                        interfaceIdentifier
+                            .Identifier
+                            .WithTrailingTrivia(WhitespaceCharacter)
+                            .WithLeadingTrivia(WhitespaceCharacter),
+                        null,
+                        null,
+                        SyntaxFactory.List<TypeParameterConstraintClauseSyntax>(),
+                        SyntaxFactory.List<MemberDeclarationSyntax>());
 
+                    var methodDeclarations = classDeclaration.Members
+                        .OfType<MethodDeclarationSyntax>()
+                        .ToArray();
+                    for (int i = 0; i < methodDeclarations.Length; i++)
+                    {
+                        var methodDeclaration = methodDeclarations[i];
+                        var externMethodKeyword = methodDeclaration.Modifiers
+                            .SingleOrDefault(x => x.IsKind(SyntaxKind.ExternKeyword));
+                        var staticMethodKeyword = methodDeclaration.Modifiers
+                            .SingleOrDefault(x => x.IsKind(SyntaxKind.StaticKeyword));
+                        if (externMethodKeyword == default(SyntaxToken) || staticMethodKeyword == default(SyntaxToken))
+                        {
+                            continue;
+                        }
+
+                        var invokeMethodIdentifier =
+                            SyntaxFactory.IdentifierName($"Invoke{methodDeclaration.Identifier.Text}");
+
+                        classDeclaration = DecorateClassWithWrapperFunction(methodDeclaration, invokeMethodIdentifier, classDeclaration);
+
+                        interfaceDeclaration = DecorateInterfaceWithWrapperFunction(methodDeclaration, invokeMethodIdentifier, interfaceDeclaration);
+                    }
+
+                    if (interfaceDeclaration.Members.Count > 0)
+                    {
+                        WriteInterfaceToFile(file, interfaceDeclaration);
+                    }
+                    if (classDeclaration.Members.Count > 0)
+                    {
+                        File.WriteAllText(
+                            file,
+                            classDeclaration.ToFullString());
+                    }
                 }
             }
+        }
+
+        private static void WriteInterfaceToFile(string file, InterfaceDeclarationSyntax interfaceDeclaration)
+        {
+            var baseFileName = Path.GetFileNameWithoutExtension(file);
+
+            var fileDirectory = Path.GetDirectoryName(file);
+            Debug.Assert(fileDirectory != null, "fileDirectory != null");
+
+            File.WriteAllText(
+                Path.Combine(
+                    fileDirectory,
+                    $"I{baseFileName}Mockable.cs"),
+                interfaceDeclaration.ToFullString());
+        }
+
+        private static InterfaceDeclarationSyntax DecorateInterfaceWithWrapperFunction(
+            MethodDeclarationSyntax methodDeclaration,
+            IdentifierNameSyntax invokeMethodIdentifier,
+            InterfaceDeclarationSyntax interfaceDeclaration)
+        {
+            var interfaceMethodDeclaration = SyntaxFactory.MethodDeclaration(
+                SyntaxFactory.List<AttributeListSyntax>(),
+                SyntaxFactory.TokenList(),
+                methodDeclaration.ReturnType,
+                default(ExplicitInterfaceSpecifierSyntax),
+                invokeMethodIdentifier.Identifier,
+                methodDeclaration.TypeParameterList,
+                methodDeclaration.ParameterList,
+                methodDeclaration.ConstraintClauses,
+                default(BlockSyntax),
+                SyntaxFactory.Token(SyntaxKind.SemicolonToken));
+
+            interfaceDeclaration = interfaceDeclaration
+                .AddMembers(
+                    interfaceMethodDeclaration
+                        .WithTrailingTrivia(NewLineCharacter)
+                        .WithLeadingTrivia(NewLineCharacter));
+            return interfaceDeclaration;
+        }
+
+        private static ClassDeclarationSyntax DecorateClassWithWrapperFunction(MethodDeclarationSyntax methodDeclaration,
+            IdentifierNameSyntax invokeMethodIdentifier,
+            ClassDeclarationSyntax classDeclaration)
+        {
+            var arguments = methodDeclaration.ParameterList
+                .Parameters
+                .Select(x => SyntaxFactory.Argument(
+                    SyntaxFactory.IdentifierName(x.Identifier)));
+
+            var arrowBody = SyntaxFactory.ArrowExpressionClause(
+                SyntaxFactory.Token(SyntaxKind.EqualsGreaterThanToken),
+                SyntaxFactory.InvocationExpression(
+                    SyntaxFactory.IdentifierName(methodDeclaration.Identifier),
+                    SyntaxFactory.ArgumentList(
+                        SyntaxFactory.SeparatedList(arguments))));
+
+            var wrapperMethodDeclaration = SyntaxFactory.MethodDeclaration(
+                SyntaxFactory.List<AttributeListSyntax>(),
+                SyntaxFactory.TokenList(),
+                methodDeclaration.ReturnType,
+                default(ExplicitInterfaceSpecifierSyntax),
+                invokeMethodIdentifier.Identifier,
+                methodDeclaration.TypeParameterList,
+                methodDeclaration.ParameterList,
+                methodDeclaration.ConstraintClauses,
+                default(BlockSyntax),
+                arrowBody);
+
+            classDeclaration = classDeclaration
+                .AddMembers(
+                    wrapperMethodDeclaration
+                        .WithTrailingTrivia(NewLineCharacter)
+                        .WithLeadingTrivia(NewLineCharacter));
+            return classDeclaration;
         }
     }
 }
