e9ff5113cf65379a974717e854d178cf6a4af361
Andrew Arnott <andrewarnott@gmail.com>
2016-01-01 11:21:47 -0800

    Add xml doc comment
    This fixes a build warning.

--- src/Windows.Core/IMAGE_SECTION_HEADER+CharacteristicsType.cs
+++ src/Windows.Core/IMAGE_SECTION_HEADER+CharacteristicsType.cs
@@ -5,8 +5,14 @@ namespace PInvoke
 {
     using System;
 
+    /// <content>
+    /// Contains the <see cref="CharacteristicsType"/> nested type.
+    /// </content>
     public partial struct IMAGE_SECTION_HEADER
     {
+        /// <summary>
+        /// Enumerates the values that may be expected in the <see cref="Characteristics"/> field.
+        /// </summary>
         [Flags]
         public enum CharacteristicsType
         {
