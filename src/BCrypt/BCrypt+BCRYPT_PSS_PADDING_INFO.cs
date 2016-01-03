328a6b8bd5302575fe3f51833bea949ea4e5a4b0
Andrew Arnott <andrewarnott@gmail.com>
2016-01-02 22:39:24 -0800

    Add the BCRYPT_PSS_PADDING_INFO struct

---
+++ src/BCrypt/BCrypt+BCRYPT_PSS_PADDING_INFO.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace PInvoke
+{
+    using System.Runtime.InteropServices;
+
+    /// <content>
+    /// Contains the <see cref="BCRYPT_PSS_PADDING_INFO"/> nested type.
+    /// </content>
+    public partial class BCrypt
+    {
+        /// <summary>
+        /// The BCRYPT_PSS_PADDING_INFO structure is used to provide options for the Probabilistic Signature Scheme (PSS) padding scheme.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct BCRYPT_PSS_PADDING_INFO
+        {
+            /// <summary>
+            /// A pointer to a null-terminated Unicode string that identifies the cryptographic algorithm to use to create the padding. This algorithm must be a hashing algorithm.
+            /// </summary>
+            public string pszAlgId;
+
+            /// <summary>
+            /// The size, in bytes, of the random salt to use for the padding.
+            /// </summary>
+            public int cbSalt;
+        }
+    }
+}
