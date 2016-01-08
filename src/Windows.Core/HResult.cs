6e080f4a6394a5561efe1115fb4112876163c98d
Andrew Arnott <andrewarnott@gmail.com>
2016-01-07 22:13:29 -0800

    Move HResult constants to their own file

--- src/Windows.Core/HResult.cs
+++ src/Windows.Core/HResult.cs
@@ -7,8 +7,6 @@ namespace PInvoke
     using System.Diagnostics;
     using System.Runtime.InteropServices;
 
-#pragma warning disable SA1124 // DoNotUseRegions
-
     /// <summary>
     /// Describes an HRESULT error or success condition.
     /// </summary>
@@ -16,70 +14,6 @@ namespace PInvoke
     [StructLayout(LayoutKind.Sequential)]
     public partial struct HResult : IComparable, IComparable<HResult>, IEquatable<HResult>, IFormattable
     {
-        #region Common HRESULT constants
-
-        /// <summary>
-        /// Operation successful, and returned a false result.
-        /// </summary>
-        public static readonly HResult S_FALSE = 1;
-
-        /// <summary>
-        /// Operation successful
-        /// </summary>
-        public static readonly HResult S_OK = 0;
-
-        /// <summary>
-        /// Unspecified failure
-        /// </summary>
-        public static readonly HResult E_FAIL = 0x80004005;
-
-        /// <summary>
-        /// Operation aborted
-        /// </summary>
-        public static readonly HResult E_ABORT = 0x80004004;
-
-        /// <summary>
-        /// General access denied error
-        /// </summary>
-        public static readonly HResult E_ACCESSDENIED = 0x80070005;
-
-        /// <summary>
-        /// Handle that is not valid
-        /// </summary>
-        public static readonly HResult E_HANDLE = 0x80070006;
-
-        /// <summary>
-        /// One or more arguments are not valid
-        /// </summary>
-        public static readonly HResult E_INVALIDARG = 0x80070057;
-
-        /// <summary>
-        /// No such interface supported
-        /// </summary>
-        public static readonly HResult E_NOINTERFACE = 0x80004002;
-
-        /// <summary>
-        /// Not implemented
-        /// </summary>
-        public static readonly HResult E_NOTIMPL = 0x80004001;
-
-        /// <summary>
-        /// Failed to allocate necessary memory
-        /// </summary>
-        public static readonly HResult E_OUTOFMEMORY = 0x8007000E;
-
-        /// <summary>
-        /// Pointer that is not valid
-        /// </summary>
-        public static readonly HResult E_POINTER = 0x80004003;
-
-        /// <summary>
-        /// Unexpected failure
-        /// </summary>
-        public static readonly HResult E_UNEXPECTED = 0x8000FFFF;
-
-        #endregion
-
         /// <summary>
         /// The mask of the bits that describe the <see cref="Facility"/>.
         /// </summary>
