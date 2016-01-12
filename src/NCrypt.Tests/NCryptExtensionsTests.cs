585334a4773638e392bc4d7f067e6f902d277012
Andrew Arnott <andrewarnott@gmail.com>
2016-01-12 07:48:12 -0800

    Add SecurityStatusException to NCrypt for failed SECURITY_STATUS codes

---
+++ src/NCrypt.Tests/NCryptExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+using System;
+using PInvoke;
+using Xunit;
+using static PInvoke.NCrypt;
+
+public class NCryptExtensionsTests
+{
+    [Fact]
+    public void GetMessage_SecurityStatus()
+    {
+        SECURITY_STATUS status = SECURITY_STATUS.NTE_BAD_DATA;
+        Assert.Equal("Bad Data", status.GetMessage());
+    }
+
+    [Fact]
+    public void ThrowOnError_OnSuccess()
+    {
+        SECURITY_STATUS.ERROR_SUCCESS.ThrowOnError();
+    }
+
+    [Fact]
+    public void ThrowOnError_Failure()
+    {
+        SECURITY_STATUS status = SECURITY_STATUS.NTE_BAD_DATA;
+        try
+        {
+            status.ThrowOnError();
+            Assert.False(true, "Expected exception not thrown.");
+        }
+        catch (SecurityStatusException ex)
+        {
+            Assert.Equal("Bad Data (SECURITY_STATUS error: NTE_BAD_DATA (0x80090005))", ex.Message);
+        }
+    }
+}
