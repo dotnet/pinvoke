5dc4bd598f59dcdce0bf013962352822881c6089
Andrew Arnott <andrewarnott@gmail.com>
2016-02-05 15:27:05 -0800

    Add User32 GetDC and ReleaseDC methods

---
+++ src/User32.Desktop/User32+SafeDCHandle.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace PInvoke
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <content>
+    /// Contains the <see cref="SafeDCHandle"/> nested type.
+    /// </content>
+    partial class User32
+    {
+        /// <summary>
+        /// A SafeHandle to track DC handles.
+        /// </summary>
+        public class SafeDCHandle : SafeHandle
+        {
+            /// <summary>
+            /// A null handle.
+            /// </summary>
+            public static readonly SafeDCHandle Null = new SafeDCHandle(IntPtr.Zero, IntPtr.Zero);
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="SafeDCHandle"/> class.
+            /// </summary>
+            /// <param name="hWnd">The HWND this handle is associated with and must be released with.</param>
+            /// <param name="hDC">The handle to the DC.</param>
+            public SafeDCHandle(IntPtr hWnd, IntPtr hDC)
+                : base(IntPtr.Zero, true)
+            {
+                this.HWnd = hWnd;
+                this.SetHandle(hDC);
+            }
+
+            /// <summary>
+            /// Gets the HWND this handle is associated with.
+            /// </summary>
+            public IntPtr HWnd { get; }
+
+            /// <inheritdoc />
+            public override bool IsInvalid => this.handle == IntPtr.Zero;
+
+            /// <inheritdoc />
+            protected override bool ReleaseHandle()
+            {
+                ReleaseDC(this.HWnd, this.handle);
+                return true;
+            }
+        }
+    }
+}
