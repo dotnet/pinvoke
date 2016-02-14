c955d2609a7d895047e93ac962cc475f32206f25
Julien Roncaglia <julien@roncaglia.fr>
2016-02-14 17:29:32 +0100

    Add initial icon structure definitions

---
+++ src/Kernel32.Desktop/Kernel32+ICONDIR.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+using System.Runtime.InteropServices;
+
+namespace PInvoke
+{
+    partial class Kernel32
+    {
+        /// <summary>
+        /// Represents a group of icons as stored in a '.ico' file
+        /// </summary>
+        /// <remarks>
+        /// The structure is followed by <see cref="idCount"/> <see cref="ICONDIRENTRY"/> entries.
+        /// </remarks>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ICONDIR
+        {
+            /// <summary>
+            /// Reserved (must be 0)
+            /// </summary>
+            public ushort idReserved;
+
+            /// <summary>
+            /// Resource type (1 for icons)
+            /// </summary>
+            public ushort idType;
+
+            /// <summary>
+            /// How many images?
+            /// </summary>
+            public ushort idCount;
+        }
+    }
+}
---
+++ src/Kernel32.Desktop/Kernel32+ICONDIRENTRY.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+using System.Runtime.InteropServices;
+
+namespace PInvoke
+{
+    partial class Kernel32
+    {
+        /// <summary>
+        /// Represents an icon as stored in a '.ico' file
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ICONDIRENTRY
+        {
+            /// <summary>
+            /// Width, in pixels, of the image
+            /// </summary>
+            public byte bWidth;
+
+            /// <summary>
+            /// Height, in pixels, of the image
+            /// </summary>
+            public byte bHeight;
+
+            /// <summary>
+            /// Number of colors in image (0 if >= 8bpp)
+            /// </summary>
+            public byte bColorCount;
+
+            /// <summary>
+            /// Reserved
+            /// </summary>
+            public byte bReserved;
+
+            /// <summary>
+            /// Color Planes
+            /// </summary>
+            public ushort wPlanes;
+
+            /// <summary>
+            /// Bits per pixel
+            /// </summary>
+            public ushort wBitCount;
+
+            /// <summary>
+            /// How many bytes in this resource
+            /// </summary>
+            public uint dwBytesInRes;
+
+            /// <summary>
+            /// Location (relative to the start of the ICO file) of the actual image data.
+            /// </summary>
+            public ushort dwImageOffset;
+        }
+    }
+}
