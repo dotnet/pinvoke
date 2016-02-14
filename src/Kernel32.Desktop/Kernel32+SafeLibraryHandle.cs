0a04f87b4b73dec646aea79f3b1726578cc71fae
Julien Roncaglia <julien@roncaglia.fr>
2016-02-14 16:52:20 +0100

    Use IntPtr.Zero instead of new IntPtr(0)

--- src/Kernel32.Desktop/Kernel32+SafeLibraryHandle.cs
+++ src/Kernel32.Desktop/Kernel32+SafeLibraryHandle.cs
@@ -16,7 +16,7 @@ namespace PInvoke
         /// </summary>
         public class SafeLibraryHandle : SafeHandle
         {
-            public static readonly SafeLibraryHandle Null = new SafeLibraryHandle(new IntPtr(0), false);
+            public static readonly SafeLibraryHandle Null = new SafeLibraryHandle(IntPtr.Zero, false);
 
             /// <summary>
             /// Initializes a new instance of the <see cref="SafeLibraryHandle"/> class.
