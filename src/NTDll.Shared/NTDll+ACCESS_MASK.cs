a4c9d33048fc7f92772f0f40f88dc4a77a6ec1ba
Andrew Arnott <andrew.arnott@microsoft.com>
2016-02-15 17:24:52 -0800

    Add ACCESS_MASK enum

---
+++ src/NTDll.Shared/NTDll+ACCESS_MASK.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace PInvoke
+{
+    /// <content>
+    /// Contains the <see cref="ACCESS_MASK"/> nested type.
+    /// </content>
+    public partial class NTDll
+    {
+        /// <summary>
+        /// The ACCESS_MASK type is a bitmask that specifies a set of access rights in the access mask of an access control entry.
+        /// </summary>
+        public enum ACCESS_MASK : uint
+        {
+            // TODO: add items
+        }
+    }
+}
