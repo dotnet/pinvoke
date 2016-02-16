ccfb196882dd58bcc1ba4230371fcdac39c6d994
Andrew Arnott <andrew.arnott@microsoft.com>
2016-02-15 17:24:52 -0800

    Add ACCESS_MASK enum

--- src/NTDll.Desktop/NTDll.cs
+++ src/NTDll.Desktop/NTDll.cs
@@ -34,7 +34,7 @@ namespace PInvoke
         [DllImport(nameof(NTDll), CharSet = CharSet.Unicode)]
         public static extern unsafe NTSTATUS NtOpenSection(
             out SafeNTObjectHandle sectionHandle,
-            uint desiredAccess,
+            ACCESS_MASK desiredAccess,
             [Friendly(FriendlyFlags.In)] OBJECT_ATTRIBUTES* objectAttributes);
 
         /// <summary>
