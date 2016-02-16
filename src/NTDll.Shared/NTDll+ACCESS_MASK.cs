238e98e37bac22b7aa043de9fbf8101e7f66437b
Andrew Arnott <andrewarnott@gmail.com>
2016-02-15 20:03:11 -0800

    Add ACCESS_MASK members

--- src/NTDll.Shared/NTDll+ACCESS_MASK.cs
+++ src/NTDll.Shared/NTDll+ACCESS_MASK.cs
@@ -13,7 +13,42 @@ namespace PInvoke
         /// </summary>
         public enum ACCESS_MASK : uint
         {
-            // TODO: add items
+            /// <summary>
+            /// Delete access.
+            /// </summary>
+            DELETE = 0x00010000,
+
+            /// <summary>
+            /// Read access to the owner, group, and discretionary access control list (DACL) of the security descriptor.
+            /// </summary>
+            READ_CONTROL = 0x00020000,
+
+            /// <summary>
+            /// Write access to the DACL.
+            /// </summary>
+            WRITE_DAC = 0x00040000,
+
+            /// <summary>
+            /// Write access to owner.
+            /// </summary>
+            WRITE_OWNER = 0x00080000,
+
+            /// <summary>
+            /// Synchronize access.
+            /// </summary>
+            SYNCHRONIZE = 0x00100000,
+
+            STANDARD_RIGHTS_REQUIRED = 0x000F0000,
+
+            STANDARD_RIGHTS_READ = READ_CONTROL,
+
+            STANDARD_RIGHTS_WRITE = READ_CONTROL,
+
+            STANDARD_RIGHTS_EXECUTE = READ_CONTROL,
+
+            STANDARD_RIGHTS_ALL = 0x001F0000,
+
+            SPECIFIC_RIGHTS_ALL = 0x0000FFFF,
         }
     }
 }
