cd3c88bec4b46b8df672e0ec9ce15adeaaf48420
Andrew Arnott <andrewarnott@gmail.com>
2016-05-15 21:45:31 -0700

    Add NetApi32 skeleton

---
+++ src/NetApi32.Desktop/NetApi32.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace PInvoke
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <content>
+    /// Exported functions from the NetApi32.dll Windows library
+    /// that are available to Desktop apps only.
+    /// </content>
+    public static partial class NetApi32
+    {
+    }
+}
