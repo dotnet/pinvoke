61b2d2f4c328698adabbd5d23609b48fdc756778
Yochay Alufer <yoalufer@microsoft.com>
2016-07-19 22:57:12 +0300

    IsProcessInJob

--- src/Kernel32.Desktop/Kernel32.cs
+++ src/Kernel32.Desktop/Kernel32.cs
@@ -1771,5 +1771,23 @@ namespace PInvoke
         /// </returns>
         [DllImport(nameof(Kernel32), SetLastError = true, CharSet = CharSet.Unicode)]
         public static extern unsafe IntPtr CreateJobObject(SECURITY_ATTRIBUTES* lpJobAttributes, string lpName);
+
+        /// <summary>
+        /// Determines whether the process is running in the specified job.
+        /// </summary>
+        /// <param name="processHandle">A handle to the process to be tested. The handle must have the PROCESS_QUERY_INFORMATION or PROCESS_QUERY_LIMITED_INFORMATION access right. For more information, see Process Security and Access Rights.
+        /// Windows Server 2003 and Windows XP:  The handle must have the PROCESS_QUERY_INFORMATION access right.
+        /// </param>
+        /// <param name="jobHandle">A handle to the job. If this parameter is NULL, the function tests if the process is running under any job.
+        /// If this parameter is not NULL, the handle must have the JOB_OBJECT_QUERY access right. For more information, see Job Object Security and Access Rights.
+        /// </param>
+        /// <param name="result">A pointer to a value that receives TRUE if the process is running in the job, and FALSE otherwise.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is nonzero.
+        /// If the function fails, the return value is zero.To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        [DllImport(nameof(Kernel32), SetLastError = true)]
+        public static extern bool IsProcessInJob(SafeObjectHandle processHandle, SafeObjectHandle jobHandle, out bool result);
     }
 }
