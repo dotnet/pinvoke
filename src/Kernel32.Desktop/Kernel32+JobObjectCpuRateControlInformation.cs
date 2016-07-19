52a9b6fadec72bf0e18e9ac454ccd938f11a4be5
Yochay Alufer <yoalufer@microsoft.com>
2016-07-20 00:19:43 +0300

    CPU rate

---
+++ src/Kernel32.Desktop/Kernel32+JobObjectCpuRateControlInformation.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace PInvoke
+{
+    /// <content>
+    /// Contains the <see cref="JOBOBJECT_CPU_RATE_CONTROL_INFORMATION "/> nested type.
+    /// </content>
+    public partial class Kernel32
+    {
+        /// <summary>
+        /// Contains CPU rate control information for a job object. This structure is used by the SetInformationJobObject and QueryInformationJobObject functions with the JobObjectCpuRateControlInformation information class.
+        /// </summary>
+        public struct JOBOBJECT_CPU_RATE_CONTROL_INFORMATION
+        {
+
+        }
+    }
+}
