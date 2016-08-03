b903e3e2d16b13905bc085cbb84ee84673666ce9
Alexandre Rocha Lima e Marcondes <amarcondes@coextant.com>
2016-08-03 22:37:38 +0200

    Adding High DPI API to SHCore

---
+++ src/SHCore.Desktop/SHCore+SHELL_UI_COMPONENT.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace PInvoke
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// Identifies the type of UI component that is needed in the shell
+    /// </summary>
+    public enum SHELL_UI_COMPONENT
+    {
+        /// <summary>
+        /// This UI component is a taskbar icon
+        /// </summary>
+        SHELL_UI_COMPONENT_TASKBARS = 0,
+
+        /// <summary>
+        /// This UI component is an icon in the notification area
+        /// </summary>
+        SHELL_UI_COMPONENT_NOTIFICATIONAREA = 1,
+
+        /// <summary>
+        /// This UI component is a deskband icon
+        /// </summary>
+        SHELL_UI_COMPONENT_DESKBAND = 2,
+    }
+}
