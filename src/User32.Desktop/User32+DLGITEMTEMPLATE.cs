c0b59b7cb4bf2ada4d845d8577284ec58aadce53
Julien Roncaglia <julien@roncaglia.fr>
2016-08-21 18:06:47 +0200

    Add a few signatures around window messages
    With this signatures we can use rc.exe to compile a windows dialog, embed
    it as a resource in a .Net executable then load and display it.
    
    There is a working minimal sample in this repository :
    https://github.com/vbfox/PInvoke.Forms.Test

---
+++ src/User32.Desktop/User32+DLGITEMTEMPLATE.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.namespace PInvoke{    using System.Runtime.InteropServices;    /// <content>    /// Contains the <see cref="DLGITEMTEMPLATE"/> nested type.    /// </content>    public partial class User32    {        /// <summary>
+        ///     Defines the dimensions and style of a control in a dialog box. One or more of these structures are combined with a
+        ///     <see cref="DLGTEMPLATE" /> structure to form a standard template for a dialog box.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, Pack = 2)]
+        public struct DLGITEMTEMPLATE
+        {
+            /// <summary>
+            ///     The style of the control. This member can be a combination of window style values (such as WS_BORDER) and one or
+            ///     more of the control style values (such as BS_PUSHBUTTON and ES_LEFT).
+            /// </summary>
+            public int style;
+
+            /// <summary>
+            ///     The extended styles for a window. This member is not used to create controls in dialog boxes, but applications that
+            ///     use dialog box templates can use it to create other types of windows.
+            /// </summary>
+            public int dwExtendedStyle;
+
+            /// <summary>
+            ///     The x-coordinate, in dialog box units, of the upper-left corner of the control. This coordinate is always relative
+            ///     to the upper-left corner of the dialog box's client area.
+            /// </summary>
+            public byte x;
+
+            /// <summary>
+            ///     The y-coordinate, in dialog box units, of the upper-left corner of the control. This coordinate is always relative
+            ///     to the upper-left corner of the dialog box's client area.
+            /// </summary>
+            public byte y;
+
+            /// <summary>
+            ///     The width, in dialog box units, of the control.
+            /// </summary>
+            public byte cx;
+
+            /// <summary>
+            ///     The height, in dialog box units, of the control.
+            /// </summary>
+            public byte cy;
+
+            /// <summary>
+            ///     The control identifier.
+            /// </summary>
+            public short id;
+        }    }}
---
+++ src/User32.Desktop/User32+MSG.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.namespace PInvoke{    using System;    using System.Runtime.InteropServices;    /// <content>    /// Contains the <see cref="MSG"/> nested type.    /// </content>    public partial class User32    {        /// <summary>
+        /// Contains message information from a thread's message queue.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MSG
+        {
+            /// <summary>
+            /// A handle to the window whose window procedure receives the message. This member is <see cref="IntPtr.Zero" /> when
+            /// the message is a thread message.
+            /// </summary>
+            public IntPtr hwnd;
+
+            /// <summary>
+            /// The message identifier. Applications can only use the low word; the high word is reserved by the system.
+            /// </summary>
+            public WindowMessage message;
+
+            /// <summary>
+            /// Additional information about the message. The exact meaning depends on the value of the message member.
+            /// </summary>
+            public IntPtr wParam;
+
+            /// <summary>
+            /// Additional information about the message. The exact meaning depends on the value of the message member.
+            /// </summary>
+            public IntPtr lParam;
+
+            /// <summary>
+            /// The time at which the message was posted.
+            /// </summary>
+            public int time;
+
+            /// <summary>
+            /// The cursor position, in screen coordinates, when the message was posted.
+            /// </summary>
+            public POINT pt;
+        }    }}
