8024d3919a73f3d68f8980ff61e288e710a11671
Julien Roncaglia <julien@roncaglia.fr>
2016-08-22 16:03:20 +0200

    Fix invalid values bug in MenuItemState
    There was no specific value defined for any member of this enum.
    
    Fixes #280

--- src/User32.Desktop/User32+MenuItemState.cs
+++ src/User32.Desktop/User32+MenuItemState.cs
@@ -3,49 +3,52 @@
 
 namespace PInvoke
 {
+    using System;
+
     /// <content>
     /// Contains the <see cref="MenuItemState"/> nested type.
     /// </content>
     public partial class User32
     {
         /// <summary>The menu item state in <see cref="MENUITEMINFO" />.</summary>
+        [Flags]
         public enum MenuItemState
         {
             /// <summary>
             ///     Checks the menu item. For more information about selected menu items, see the
             ///     <see cref="MENUITEMINFO.hbmpChecked" /> member.
             /// </summary>
-            MFS_CHECKED,
+            MFS_CHECKED = MenuItemFlags.MF_CHECKED,
 
             /// <summary>
             ///     Specifies that the menu item is the default. A menu can contain only one default menu item, which is displayed
             ///     in bold.
             /// </summary>
-            MFS_DEFAULT,
+            MFS_DEFAULT = MenuItemFlags.MF_DEFAULT,
 
             /// <summary>
             ///     Disables the menu item and grays it so that it cannot be selected. This is equivalent to
             ///     <see cref="MFS_GRAYED" />.
             /// </summary>
-            MFS_DISABLED,
+            MFS_DISABLED = MFS_GRAYED,
 
             /// <summary>Enables the menu item so that it can be selected. This is the default state.</summary>
-            MFS_ENABLED,
+            MFS_ENABLED = MenuItemFlags.MF_ENABLED,
 
             /// <summary>
             ///     Disables the menu item and grays it so that it cannot be selected. This is equivalent to
             ///     <see cref="MFS_DISABLED" />.
             /// </summary>
-            MFS_GRAYED,
+            MFS_GRAYED = 0x00000003,
 
             /// <summary>Highlights the menu item.</summary>
-            MFS_HILITE,
+            MFS_HILITE = MenuItemFlags.MF_HILITE,
 
             /// <summary>Unchecks the menu item. For more information about clear menu items, see the hbmpChecked member.</summary>
-            MFS_UNCHECKED,
+            MFS_UNCHECKED = MenuItemFlags.MF_UNCHECKED,
 
             /// <summary>Removes the highlight from the menu item. This is the default state.</summary>
-            MFS_UNHILITE
+            MFS_UNHILITE = MenuItemFlags.MF_UNHILITE
         }
     }
 }
