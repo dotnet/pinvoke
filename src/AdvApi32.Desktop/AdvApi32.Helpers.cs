bf30bd97dfedb1ff2a9c75e5872b629c3a527e2e
Alexandre Rocha Lima e Marcondes <amarcondes@coextant.com>
2016-09-09 21:35:57 +0200

    Strange thing. It's the only way to make it compile. The void* pointers are going crazy!!!

--- src/AdvApi32.Desktop/AdvApi32.Helpers.cs
+++ src/AdvApi32.Desktop/AdvApi32.Helpers.cs
@@ -246,8 +246,9 @@ namespace PInvoke
 
                     fixed (void* lpInfo = new byte[Marshal.SizeOf(descriptionStruct)])
                     {
-                        Marshal.StructureToPtr(descriptionStruct, new IntPtr(lpInfo), false);
-                        if (!ChangeServiceConfig2(svcHandle, ServiceInfoLevel.SERVICE_CONFIG_DESCRIPTION, lpInfo))
+                        var ptr = new IntPtr(lpInfo);
+                        Marshal.StructureToPtr(descriptionStruct, ptr, false);
+                        if (!ChangeServiceConfig2(svcHandle, ServiceInfoLevel.SERVICE_CONFIG_DESCRIPTION, ptr))
                         {
                             throw new Win32Exception();
                         }
@@ -298,14 +299,14 @@ namespace PInvoke
             }
         }
 
-        /// <summary>Get the elevation type of a token via <see cref="GetTokenInformation(SafeObjectHandle, TOKEN_INFORMATION_CLASS, void*, int, out int)" />.</summary>
+        /// <summary>Get the elevation type of a token via <see cref="GetTokenInformation(SafeObjectHandle, TOKEN_INFORMATION_CLASS, IntPtr, int, out int)" />.</summary>
         /// <param name="TokenHandle">
         ///     A handle to an access token from which information is retrieved. The handle must have
         ///     TOKEN_QUERY access.
         /// </param>
         /// <returns>The token elevation type</returns>
         /// <exception cref="ArgumentNullException"><paramref name="TokenHandle" /> is NULL.</exception>
-        /// <exception cref="Win32Exception">If the call to <see cref="GetTokenInformation(SafeObjectHandle, TOKEN_INFORMATION_CLASS, void*, int, out int)" /> fails.</exception>
+        /// <exception cref="Win32Exception">If the call to <see cref="GetTokenInformation(SafeObjectHandle, TOKEN_INFORMATION_CLASS, IntPtr, int, out int)" /> fails.</exception>
         public static TOKEN_ELEVATION_TYPE GetTokenElevationType(SafeObjectHandle TokenHandle)
         {
             if (TokenHandle == null)
@@ -322,7 +323,7 @@ namespace PInvoke
                 success = GetTokenInformation(
                     TokenHandle,
                     TOKEN_INFORMATION_CLASS.TokenElevationType,
-                    &elevationType,
+                    new IntPtr(&elevationType),
                     sizeof(TOKEN_ELEVATION_TYPE),
                     out returnLength);
             }
