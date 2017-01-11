cf61f531bd3a6e4eadb50060125e5e8a435fd6f8
Andrew Arnott <andrewarnott@gmail.com>
2017-01-10 18:12:19 -0800

    Add INPUT union memory alignment test

--- src/User32.Tests/User32Facts.cs
+++ src/User32.Tests/User32Facts.cs
@@ -35,4 +35,27 @@ public partial class User32Facts
         Assert.Equal(1, (int)i.Inputs.ki.wVk);
         Assert.Equal(1, i.Inputs.mi.dx);
     }
+
+    /// <summary>
+    /// Validates that the union fields are all exactly 4 bytes (or 8 bytes on x64)
+    /// after the start of the struct.i
+    /// </summary>
+    /// <devremarks>
+    /// From http://www.pinvoke.net/default.aspx/Structures/INPUT.html:
+    /// The last 3 fields are a union, which is why they are all at the same memory offset.
+    /// On 64-Bit systems, the offset of the <see cref="mi"/>, <see cref="ki"/> and <see cref="hi"/> fields is 8,
+    /// because the nested struct uses the alignment of its biggest member, which is 8
+    /// (due to the 64-bit pointer in <see cref="KEYBDINPUT.dwExtraInfo"/>).
+    /// By separating the union into its own structure, rather than placing the
+    /// <see cref="mi"/>, <see cref="ki"/> and <see cref="hi"/> fields directly in the INPUT structure,
+    /// we assure that the .NET structure will have the correct alignment on both 32 and 64 bit.
+    /// </devremarks>
+    [Fact]
+    public unsafe void INPUT_UnionMemoryAlignment()
+    {
+        INPUT input;
+        Assert.Equal(IntPtr.Size, (long)&input.Inputs.hi - (long)&input);
+        Assert.Equal(IntPtr.Size, (long)&input.Inputs.mi - (long)&input);
+        Assert.Equal(IntPtr.Size, (long)&input.Inputs.ki - (long)&input);
+    }
 }
