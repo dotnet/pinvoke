d227e91c02038899cf54526d6b69667ac689be7f
Andrew Arnott <andrewarnott@gmail.com>
2017-03-05 19:19:04 -0800

    Fix stylecop issues in debugging project

--- src/CodeGeneration.Debugging/Program.cs
+++ src/CodeGeneration.Debugging/Program.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 
 namespace CodeGeneration.Debugging
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // This app is a dummy. But when it is debugged within VS, it builds the Tests
             // allowing VS to debug into the build/code generation process.
