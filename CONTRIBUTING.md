a030e8bcba0a10f9dda6eb0064fd4de4e418f196
Andrew Arnott <andrewarnott@gmail.com>
2017-03-07 08:04:36 -0800

    Update templates

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -8,7 +8,10 @@ Please send pull requests to add what you've come up with.
 
 ### Required
 
-* [Visual Studio 2017](https://www.visualstudio.com/en-us)
+* [Visual Studio 2017](https://www.visualstudio.com/en-us) with the following workloads:
+  * Desktop Development with C++
+  * Desktop Development with .NET
+  * .NET Core
 
 ## Guidelines
 
--- templates/AddNewLibrary.ps1
+++ templates/AddNewLibrary.ps1
@@ -14,44 +14,17 @@ param(
     [Parameter(Mandatory=$true,Position=0)]
     [string]$LibraryName
 )
-function Replace-Placeholders {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]$Path,
-        [Parameter(Mandatory=$true)]
-        [string]$LibraryName,
-        [Parameter(Mandatory=$true)]
-        $Replacements
-    )
 
-    Get-ChildItem -Recurse $Path -File |% {
-        $content = Get-Content -Path $_.FullName
-        $Replacements.GetEnumerator() |% { $content = $content -replace $_.Key,$_.Value }
-        Set-Content -Path $_.FullName -Value $content -Encoding UTF8
-
-        if ($_.Name -match 'LIBNAME') {
-            $NewLeafName = $_.Name -Replace 'LIBNAME',$LibraryName
-            Rename-Item $_.FullName $NewLeafName
-        }
-    }
-
-    # Rename directories
-    Get-ChildItem -Recurse $Path -Directory |% {
-        if ($_.Name -match 'LIBNAME') {
-            $NewLeafName = $_.Name -Replace 'LIBNAME',$LibraryName
-            Rename-Item $_.FullName $NewLeafName
-        }
-    }
-
-    # Finally, rename $Path itself
-    $NewLeafName = (Split-Path -Leaf $Path) -Replace 'LIBNAME',$LibraryName
-    Rename-Item $Path $NewLeafName
+if (!(Get-Command dumpbin)) {
+    Write-Error "DUMPBIN must be on your path. Install the `"Desktop Development with C++`" workload and repeat this command within a VS 2017 Developer Command Prompt."
+    return 1
 }
+
+. $PSScriptRoot\Replace-Placeholders.ps1
 
 $Src = Resolve-Path "$PSScriptRoot\..\src"
 
-$Directories = 'LIBNAME','LIBNAME.Profile111','LIBNAME.Desktop','LIBNAME.Shared','LIBNAME.Tests','LIBNAME.NuGet'
+$Directories = 'LIBNAME','LIBNAME.Tests'
 $TemplateDirectories = @()
 $SrcDirectories = @()
 foreach($dir in $Directories) {
@@ -65,28 +38,18 @@ foreach($dir in $Directories) {
 }
 
 $Replacements = @{
-    '\$guid1\$' = [Guid]::NewGuid().ToString('b').ToUpper();
-    '\$guid2\$' = [Guid]::NewGuid().ToString('b').ToUpper();
-    '\$guid3\$' = [Guid]::NewGuid().ToString('b').ToUpper();
-    '\$guid4\$' = [Guid]::NewGuid().ToString('b').ToUpper();
-    '\$guid5\$' = [Guid]::NewGuid().ToString('b').ToUpper();
-    '\$guid6\$' = [Guid]::NewGuid().ToString('b').ToUpper();
     'LIBNAME' = $LibraryName;
 }
 
 Copy-Item -Recurse -Path $TemplateDirectories -Destination $Src
 $SrcDirectories |% { Replace-Placeholders -LibraryName $LibraryName -Replacements $Replacements -Path $_ }
 
-& "$PSScriptRoot\CreateExportsTxtFile.ps1" -AssemblyPath "$env:windir\System32\$LibraryName.dll" -OutputDir "$Src\$LibraryName.Desktop\"
+& "$PSScriptRoot\CreateExportsTxtFile.ps1" -AssemblyPath "$env:windir\System32\$LibraryName.dll" -OutputDir "$Src\$LibraryName\"
 
 Write-Output "Great. Your new projects have been created. Please also perform a few more manual steps:"
 Write-Output "1. Add these new projects to your solution file:"
 Write-Output "    $Src\$LibraryName\$LibraryName.csproj"
-Write-Output "    $Src\$LibraryName.Profile111\$LibraryName.Profile111.csproj"
-Write-Output "    $Src\$LibraryName.Desktop\$LibraryName.Desktop.csproj"
-Write-Output "    $Src\$LibraryName.Shared\$LibraryName.Shared.shproj"
 Write-Output "    $Src\$LibraryName.Tests\$LibraryName.Tests.csproj"
-Write-Output "    $Src\$LibraryName.NuGet\$LibraryName.NuGet.nuproj"
 Write-Output "2. Add your library to the README.md file."
-Write-Output "3. Add a project reference to $Src\$LibraryName.NuGet\$LibraryName.NuGet.nuproj"
-Write-Output "   into the PInvoke.Win32.nuproj project, if it's part of the Win32 API."
+Write-Output "3. Add a project reference to $Src\$CoreLibraryName\$CoreLibraryName.csproj"
+Write-Output "   into the Win32 project, if it's part of the Win32 API."
