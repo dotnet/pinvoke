05034d827ec5169d8ebe293702879358b106ced7
Andrew Arnott <andrewarnott@gmail.com>
2017-03-07 10:56:57 -0800

    Fix up build and test runner

--- build.ps1
+++ build.ps1
@@ -10,7 +10,7 @@
 .Parameter Test
     Run all built tests.
 .Parameter Configuration
-    The configuration to build. Either "debug" or "release". The default is debug.
+    The configuration to build. Either "debug" or "release". The default is debug, or the Configuration environment variable if set.
 .Parameter WarnAsError
     Converts all build warnings to errors. Useful in preparation to sending a pull request.
 #>
@@ -20,9 +20,11 @@ Param(
     [switch]$Build,
     [switch]$Test,
     [Parameter()][ValidateSet('debug', 'release')]
-    [string]$Configuration = 'debug',
+    [string]$Configuration = $env:configuration,
     [switch]$WarnAsError = $true
 )
+
+if (!$Configuration) { $Configuration = 'debug' }
 
 $NothingToDo = !($Restore -or $Build -or $Test)
 if ($NothingToDo) {
@@ -42,21 +44,16 @@ $SolutionFile = Join-Path $SolutionFolder "PInvoke.sln"
 $ToolsFolder = Join-Path $ProjectRoot tools
 $BinFolder = Join-Path $ProjectRoot "bin"
 $BinConfigFolder = Join-Path $BinFolder $Configuration
-$BinTestsFolder = Join-Path $BinConfigFolder "net46"
+$BinTestsFolder = Join-Path $BinConfigFolder "tests"
+$PackageRestoreRoot = Join-Path $env:userprofile '.nuget/packages/'
 
 # Set script scope for external tool variables.
 $MSBuildCommand = Get-Command MSBuild.exe -ErrorAction SilentlyContinue
-$VSTestConsoleCommand = Get-Command vstest.console.exe -ErrorAction SilentlyContinue
 
 Function Get-ExternalTools {
     if (!$MSBuildCommand) {
         Write-Error "Unable to find MSBuild.exe. Make sure you're running in a VS Developer Prompt."
         exit 1;
-    }
-
-    if (!$VSTestConsoleCommand) {
-        Write-Error "Unable to find vstest.console.exe. Make sure you're running in a VS Developer prompt."
-        exit 2;
     }
 }
 
@@ -69,7 +66,7 @@ if ($Restore -and $PSCmdlet.ShouldProcess($SolutionFile, "Restore packages")) {
 
 if ($Build -and $PSCmdlet.ShouldProcess($SolutionFile, "Build")) {
     Write-Output "Building..."
-    & $MSBuildCommand.Path $SolutionFile /nologo /nr:false /m /v:minimal /fl /t:pack "/flp:verbosity=normal;logfile=msbuild.log" "/flp1:warningsonly;logfile=msbuild.wrn;NoSummary;verbosity=minimal" "/flp2:errorsonly;logfile=msbuild.err;NoSummary;verbosity=minimal"
+    & $MSBuildCommand.Path $SolutionFile /nologo /nr:false /m /v:minimal /fl /t:build,pack "/flp:verbosity=normal;logfile=msbuild.log" "/flp1:warningsonly;logfile=msbuild.wrn;NoSummary;verbosity=minimal" "/flp2:errorsonly;logfile=msbuild.err;NoSummary;verbosity=minimal"
     $fail = $false
 
     $warnings = Get-Content msbuild.wrn
@@ -97,9 +94,9 @@ if ($Build -and $PSCmdlet.ShouldProcess($SolutionFile, "Build")) {
     }
 }
 
-if ($Test -and $PSCmdlet.ShouldProcess('Test assemblies', 'vstest.console.exe')) {
+if ($Test -and $PSCmdlet.ShouldProcess('Test assemblies')) {
     $TestAssemblies = Get-ChildItem -Recurse "$BinTestsFolder\*.Tests.dll"
     Write-Output "Testing..."
-    # Add /Parallel switch when VS2015 Update 2 is more popular, as it's new in that version.
-    & $VSTestConsoleCommand.Path /Logger:trx /TestAdapterPath:$BinTestsFolder $TestAssemblies
+    $xunitRunner = Join-Path $PackageRestoreRoot 'xunit.runner.console/2.2.0/tools/xunit.console.x86.exe'
+    & $xunitRunner $TestAssemblies -nologo -html "$BinTestsFolder\testresults.html" -xml "$BinTestsFolder\testresults.xml" -parallel all
 }
