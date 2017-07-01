6ff58acfe312dadafe59d5309c6d739cbbdd3c21
Andrew Arnott <andrewarnott@gmail.com>
2017-07-01 12:06:32 -0700

    Add DestroyWindow

--- src/User32/User32.cs
+++ src/User32/User32.cs
@@ -2574,6 +2574,20 @@ namespace PInvoke
            IntPtr hInstance,
            void* lpParam);
 
+        /// <summary>
+        /// Destroys the specified window. The function sends WM_DESTROY and WM_NCDESTROY messages to the window to deactivate it and remove the keyboard focus from it. The function also destroys the window's menu, flushes the thread message queue, destroys timers, removes clipboard ownership, and breaks the clipboard viewer chain (if the window is at the top of the viewer chain).
+        /// If the specified window is a parent or owner window, DestroyWindow automatically destroys the associated child or owned windows when it destroys the parent or owner window. The function first destroys child or owned windows, and then it destroys the parent or owner window.
+        /// DestroyWindow also destroys modeless dialog boxes created by the CreateDialog function.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window to be destroyed. </param>
+        /// <returns>
+        /// If the function succeeds, the return value is nonzero.
+        /// If the function fails, the return value is zero. To get extended error information, call GetLastError.
+        /// </returns>
+        [DllImport(nameof(User32), SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DestroyWindow(IntPtr hWnd);
+
         [DllImport(nameof(User32), SetLastError = true)]
         public static extern IntPtr DispatchMessage(ref MSG lpmsg);
 
