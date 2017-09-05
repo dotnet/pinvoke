1f39aa3acff4706c51cb1f7b3b5a2a42f55dc526
Barry Nolte <barry@barrynolte.com>
2017-09-05 16:15:44 -0700

    Uncommented/deleted code
    Used the commented out DllImport and deleted the unneeded delaration.

--- src/User32/User32.cs
+++ src/User32/User32.cs
@@ -253,10 +253,7 @@ namespace PInvoke
         /// <param name="idAttachTo">The identifier of the thread to which idAttach will be attached.This thread cannot be a system thread. A thread cannot attach to itself. Therefore, idAttachTo cannot equal idAttach.</param>
         /// <param name="fAttach">If this parameter is TRUE, the two threads are attached.If the parameter is FALSE, the threads are detached.</param>
         /// <returns>If the function succeeds, the return value is nonzero. If the function fails, the return value is zero.To get extended error information, call GetLastError. Windows Server2003 and WindowsXP:  There is no extended error information; do not call GetLastError.This behavior changed as of WindowsVista.</returns>
-        // [DllImport(nameof(User32), SetLastError = true)]
-        // public static extern bool AttachThreadInput(int idAttach, int idAttachTo, bool fAttach);
-        [DllImport("user32.dll", EntryPoint = "AttachThreadInput")]
-        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport(nameof(User32), SetLastError = true)]
         public static extern bool AttachThreadInput(int idAttach, int idAttachTo, [MarshalAs(UnmanagedType.Bool)] bool fAttach);
 
         [DllImport(nameof(User32), SetLastError = true)]
