1628ad24df646249f288683f2977a9acee7f5326
Andrew Arnott <andrewarnott@gmail.com>
2018-04-17 08:41:09 -0700

    Fix line endings

--- src/User32/User32+DLGTEMPLATE.cs
+++ src/User32/User32+DLGTEMPLATE.cs
@@ -1,4 +1,5 @@
-﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+﻿// Copyright (c) to owners found in https://github.com/AArnott/pinvoke/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace PInvoke
 {
