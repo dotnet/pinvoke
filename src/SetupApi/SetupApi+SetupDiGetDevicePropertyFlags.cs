aac01e3bf8432506cd607f7e31dfcc530fd1841f
Andrew Arnott <Andrew.arnott@Microsoft.com>
2019-12-22 07:11:04 -0700

    Merge pull request #443 from AArnott/fix442
    Add SetupDiGetDeviceProperty to SetupApi

---
+++ src/SetupApi/SetupApi+SetupDiGetDevicePropertyFlags.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) All contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace PInvoke
+{
+    using System;
+
+    /// <content>
+    /// Contains the <see cref="SetupDiGetDevicePropertyFlags"/> nested type.
+    /// </content>
+    public partial class SetupApi
+    {
+        /// <summary>
+        /// Flags for the <see cref="SetupDiGetDeviceProperty(SafeDeviceInfoSetHandle, SP_DEVINFO_DATA*, DEVPROPKEY*, uint*, byte*, uint, uint*, SetupDiGetDevicePropertyFlags)"/> method.
+        /// </summary>
+        [Flags]
+        public enum SetupDiGetDevicePropertyFlags
+        {
+            /// <summary>
+            /// No flags.
+            /// </summary>
+            None = 0,
+        }
+    }
+}
