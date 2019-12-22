832944cd30473c87b5d8ecd656275ee0a68e11ce
Andrew Arnott <andrewarnott@gmail.com>
2019-12-22 10:46:06 -0700

    Update to .NET Core SDK 3.1.100

--- build.cmd
+++ build.cmd
@@ -1,1 +1,4 @@
-@PowerShell.exe -ExecutionPolicy RemoteSigned "%~dpn0.ps1" %*
+@echo off
+SETLOCAL
+set PS1UnderCmd=1
+powershell.exe -NoProfile -NoLogo -ExecutionPolicy bypass -Command "try { & '%~dpn0.ps1' %*; exit $LASTEXITCODE } catch { write-host $_; exit 1 }"
