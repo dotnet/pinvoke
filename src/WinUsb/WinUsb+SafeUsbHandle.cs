3eca8f13d08aec6f31872661c96bd7aa7f61e9e9
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-07-01 11:51:45 +0200

    Add WinUsb API

---
+++ src/WinUsb/WinUsb+SafeUsbHandle.cs
@@ -0,0 +1,46 @@
+﻿// Copyright © .NET Foundation and Contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace PInvoke
+{
+    using System;
+    using Microsoft.Win32.SafeHandles;
+
+    /// <content>
+    /// Contains the nested <see cref="SafeUsbHandle"/> type.
+    /// </content>
+    public static partial class WinUsb
+    {
+        /// <summary>
+        /// A handle which is returned by the WinUsb API.
+        /// </summary>
+        public class SafeUsbHandle : SafeHandleMinusOneIsInvalid
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="SafeUsbHandle"/> class.
+            /// </summary>
+            public SafeUsbHandle()
+                : base(true)
+            {
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="SafeUsbHandle"/> class.
+            /// </summary>
+            /// <param name="handle">
+            /// The predefined handle.
+            /// </param>
+            public SafeUsbHandle(IntPtr handle)
+                : this()
+            {
+                this.SetHandle(handle);
+            }
+
+            /// <inheritdoc/>
+            protected override bool ReleaseHandle()
+            {
+                return WinUsb.WinUsb_Free(this.handle);
+            }
+        }
+    }
+}
