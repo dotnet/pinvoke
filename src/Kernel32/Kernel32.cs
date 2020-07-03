c392472c8f49e2fabad2748524656d15162488b4
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2020-07-03 12:15:19 -0700

    Add GetCurrentThread

--- src/Kernel32/Kernel32.cs
+++ src/Kernel32/Kernel32.cs
@@ -916,6 +916,33 @@ namespace PInvoke
             [Friendly(FriendlyFlags.Out)] PROCESSOR_NUMBER* ProcNumber);
 
         /// <summary>
+        /// Retrieves a pseudo handle for the calling thread.
+        /// </summary>
+        /// <returns>The return value is a pseudo handle for the current thread.</returns>
+        /// <remarks>
+        /// A pseudo handle is a special constant that is interpreted as the current thread handle. The calling thread can use this handle to specify itself
+        /// whenever a thread handle is required. Pseudo handles are not inherited by child processes.
+        ///
+        /// This handle has the THREAD_ALL_ACCESS access right to the thread object.
+        ///
+        /// Windows Server 2003 and Windows XP:  This handle has the maximum access allowed by the security descriptor of the thread to the primary token of
+        /// the process.
+        ///
+        /// The function cannot be used by one thread to create a handle that can be used by other threads to refer to the first thread.The handle is always
+        /// interpreted as referring to the thread that is using it.A thread can create a "real" handle to itself that can be used by other threads, or
+        /// inherited by other processes, by specifying the pseudo handle as the source handle in a call to the DuplicateHandle function.
+        ///
+        /// The pseudo handle need not be closed when it is no longer needed.Calling the <see cref="CloseHandle(IntPtr)"/> function with this handle has no effect. If the pseudo
+        /// handle is duplicated by DuplicateHandle, the duplicate handle must be closed.
+        ///
+        /// Do not create a thread while impersonating a security context. The call will succeed, however the newly created thread will have reduced access
+        /// rights to itself when calling GetCurrentThread.The access rights granted this thread will be derived from the access rights the impersonated user
+        /// has to the process.Some access rights including THREAD_SET_THREAD_TOKEN and THREAD_GET_CONTEXT may not be present, leading to unexpected failures.
+        /// </remarks>
+        [DllImport(nameof(Kernel32))]
+        public static extern SafeObjectHandle GetCurrentThread();
+
+        /// <summary>
         ///     Closes a file search handle opened by the FindFirstFile, FindFirstFileEx, FindFirstFileNameW,
         ///     FindFirstFileNameTransactedW, FindFirstFileTransacted, FindFirstStreamTransactedW, or FindFirstStreamW functions.
         /// </summary>
