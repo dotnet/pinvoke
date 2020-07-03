f15e50d45b73bf5b91015a9eb9674c045268aee0
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2020-07-03 15:28:10 -0700

    Add GetPriorityClass

--- src/Kernel32/Kernel32.cs
+++ src/Kernel32/Kernel32.cs
@@ -960,6 +960,37 @@ namespace PInvoke
             [Friendly(FriendlyFlags.Out)] ulong* HighLimit);
 
         /// <summary>
+        /// Retrieves the priority class for the specified process. This value, together with the priority value of each thread of the process,
+        /// determines each thread's base priority level.
+        /// </summary>
+        /// <param name="hProcess">
+        /// A handle to the process.
+        ///
+        /// The handle must have the <see cref="ProcessAccess.PROCESS_QUERY_INFORMATION"/> or <see cref="ProcessAccess.PROCESS_QUERY_LIMITED_INFORMATION"/> access
+        /// right.For more information,see  <a href="https://docs.microsoft.com/en-us/windows/desktop/ProcThread/process-security-and-access-rights">Process Security and Access Rights</a>.
+        ///
+        /// Windows Server 2003 and Windows XP:  The handle must have the <see cref="ProcessAccess.PROCESS_QUERY_INFORMATION"/> access right.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is the priority class of the specified process.
+        ///
+        /// If the function fails, the return value is zero.To get extended error information, call <see cref="Marshal.GetLastWin32Error"/>.
+        /// </returns>
+        /// <remarks>
+        /// Every thread has a base priority level determined by the thread's priority value and the priority class of its process. The operating system
+        /// uses the base priority level of all executable threads to determine which thread gets the next slice of CPU time. Threads are scheduled in a
+        /// round-robin fashion at each priority level, and only when there are no executable threads at a higher level will scheduling of threads at a
+        /// lower level take place.
+        ///
+        /// For a table that shows the base priority levels for each combination of priority class and thread priority value, see
+        /// <a href="https://docs.microsoft.com/en-us/windows/desktop/ProcThread/scheduling-priorities">Scheduling Priorities</a>
+        ///
+        /// Priority class is maintained by the executive, so all processes have a priority class that can be queried.
+        /// </remarks>
+        [DllImport(nameof(Kernel32), SetLastError = true)]
+        public static extern ProcessPriorityClass GetPriorityClass(SafeObjectHandle hProcess);
+
+        /// <summary>
         ///     Closes a file search handle opened by the FindFirstFile, FindFirstFileEx, FindFirstFileNameW,
         ///     FindFirstFileNameTransactedW, FindFirstFileTransacted, FindFirstStreamTransactedW, or FindFirstStreamW functions.
         /// </summary>
