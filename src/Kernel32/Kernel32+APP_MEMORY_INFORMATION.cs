96c4aac1a134a4ac14185d3b337a18ed2888afaa
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2020-07-04 16:57:52 -0700

    Added APP_MEMORY_INFORMATION struct

---
+++ src/Kernel32/Kernel32+APP_MEMORY_INFORMATION.cs
@@ -0,0 +1,40 @@
+﻿// Copyright © .NET Foundation and Contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace PInvoke
+{
+    /// <content>
+    /// Contains the <see cref="APP_MEMORY_INFORMATION"/> nested type.
+    /// </content>
+    public partial class Kernel32
+    {
+        /// <summary>
+        /// Represents app memory usage at a single point in time.
+        /// This structure is used by the <see cref="PROCESS_INFORMATION_CLASS"/> class.
+        /// </summary>
+        public struct APP_MEMORY_INFORMATION
+        {
+#pragma warning disable IDE1006 // Naming Styles
+            /// <summary>
+            /// Total commit available to the app
+            /// </summary>
+            public ulong AvailableCommit;
+
+            /// <summary>
+            /// The app's usage of private commit
+            /// </summary>
+            public ulong PrivateCommitUsage;
+
+            /// <summary>
+            /// The app's peak usage of private commit
+            /// </summary>
+            public ulong PeakPrivateCommitUsage;
+
+            /// <summary>
+            /// The app's total usage of private plus shared commit
+            /// </summary>
+            public ulong TotalCommitUsage;
+#pragma warning restore IDE1006 // Naming Styles
+        }
+    }
+}
