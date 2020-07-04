9ff0bc343041117af2b079eea3ecb06774c6f7ce
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2020-07-04 15:35:20 -0700

    Add MEMORY_PRIORITY_INFORMATION struct

---
+++ src/Kernel32/Kernel32+MEMORY_PRIORITY_INFORMATION.cs
@@ -0,0 +1,26 @@
+﻿// Copyright © .NET Foundation and Contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace PInvoke
+{
+    /// <content>
+    /// Contains the <see cref="MEMORY_PRIORITY_INFORMATION"/> nested type.
+    /// </content>
+    public partial class Kernel32
+    {
+        /// <summary>
+        /// Specifies the memory priority for a thread or process.
+        /// This structure is used by the GetProcessInformation, SetProcessInformation, GetThreadInformation, and SetThreadInformation
+        /// functions.
+        /// </summary>
+        public struct MEMORY_PRIORITY_INFORMATION
+        {
+#pragma warning disable IDE1006 // Naming Styles
+            /// <summary>
+            /// The memory priority for the thread or process
+            /// </summary>
+            public MemoryPriority MemoryPriority;
+#pragma warning restore IDE1006 // Naming Styles
+        }
+    }
+}
