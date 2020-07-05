d01ff73a93ee38199f79b001262f39a4534b1d69
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2020-07-04 17:44:25 -0700

    Add PROCESS_LEAP_SECOND_INFO struct

---
+++ src/Kernel32/Kernel32+PROCESS_LEAP_SECOND_INFO.cs
@@ -0,0 +1,29 @@
+﻿// Copyright © .NET Foundation and Contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace PInvoke
+{
+    /// <content>
+    /// Contains the <see cref="PROCESS_LEAP_SECOND_INFO"/> nested type.
+    /// </content>
+    public partial class Kernel32
+    {
+        /// <summary>
+        /// Specifies how the system handles positive leap seconds
+        /// </summary>
+        public struct PROCESS_LEAP_SECOND_INFO
+        {
+#pragma warning disable IDE1006 // Naming Styles
+            /// <summary>
+            /// Flag specifying how the system handles leap seconds
+            /// </summary>
+            public ProcessLeapSecondInfoFlags Flags;
+
+            /// <summary>
+            /// Reserved for future use
+            /// </summary>
+            public uint Reserved;
+#pragma warning restore IDE1006 // Naming Styles
+        }
+    }
+}
