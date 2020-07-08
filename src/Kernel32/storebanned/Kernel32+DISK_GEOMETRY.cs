60cfcfc586bfb1a52f2e48249d7c087a9fd68db8
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-07-08 22:35:28 +0200

    Add DISK_GEOMETRY struct, MEDIA_TYPE enum

---
+++ src/Kernel32/storebanned/Kernel32+DISK_GEOMETRY.cs
@@ -0,0 +1,43 @@
+﻿// Copyright © .NET Foundation and Contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace PInvoke
+{
+    /// <content>
+    /// Contains the <see cref="DISK_GEOMETRY"/> nested type.
+    /// </content>
+    public partial class Kernel32
+    {
+        /// <summary>
+        /// Describes the geometry of disk devices and media.
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/windows/win32/api/winioctl/ns-winioctl-disk_geometry"/>
+        public struct DISK_GEOMETRY
+        {
+            /// <summary>
+            /// The number of cylinders.
+            /// </summary>
+            public long Cylinders;
+
+            /// <summary>
+            /// The type of media.
+            /// </summary>
+            public MEDIA_TYPE MediaType;
+
+            /// <summary>
+            /// The number of tracks per cylinder.
+            /// </summary>
+            public uint TracksPerCylinder;
+
+            /// <summary>
+            /// The number of sectors per track.
+            /// </summary>
+            public uint SectorsPerTrack;
+
+            /// <summary>
+            /// The number of bytes per sector.
+            /// </summary>
+            public uint BytesPerSector;
+        }
+    }
+}
