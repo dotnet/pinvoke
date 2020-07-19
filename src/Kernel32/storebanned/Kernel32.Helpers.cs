53b9104e62ea605ca7c1b1ad35dd85254b32bb0e
Andrew Arnott <andrewarnott@gmail.com>
2020-07-19 12:56:24 -0600

    Reuse xml doc comments via inheritdoc

--- src/Kernel32/storebanned/Kernel32.Helpers.cs
+++ src/Kernel32/storebanned/Kernel32.Helpers.cs
@@ -383,58 +383,7 @@ namespace PInvoke
             }
         }
 
-        /// <summary>
-        /// Retrieves the results of an overlapped operation on the specified file, named pipe, or communications device.
-        /// To specify a timeout interval or wait on an alertable thread, use GetOverlappedResultEx.
-        /// </summary>
-        /// <param name="hFile">
-        /// A handle to the file, named pipe, or communications device. This is the same handle that was
-        /// specified when the overlapped operation was started by a call to the ReadFile, WriteFile, ConnectNamedPipe,
-        /// TransactNamedPipe, DeviceIoControl, or WaitCommEvent function.
-        /// </param>
-        /// <param name="lpOverlapped">
-        /// A pointer to an <see cref="NativeOverlapped" /> structure that was specified when the overlapped
-        /// operation was started.
-        /// </param>
-        /// <param name="lpNumberOfBytesTransferred">
-        /// A pointer to a variable that receives the number of bytes that were actually
-        /// transferred by a read or write operation. For a TransactNamedPipe operation, this is the number of bytes that were read
-        /// from the pipe. For a DeviceIoControl operation, this is the number of bytes of output data returned by the device
-        /// driver. For a ConnectNamedPipe or WaitCommEvent operation, this value is undefined.
-        /// </param>
-        /// <param name="bWait">
-        /// If this parameter is TRUE, and the Internal member of the lpOverlapped structure is STATUS_PENDING,
-        /// the function does not return until the operation has been completed. If this parameter is FALSE and the operation is
-        /// still pending, the function returns FALSE and the <see cref="GetLastError" /> function returns
-        /// <see cref="Win32ErrorCode.ERROR_IO_INCOMPLETE" />.
-        /// </param>
-        /// <returns>
-        /// If the function succeeds, the return value is nonzero.
-        /// <para>
-        /// If the function fails, the return value is zero.To get extended error information, call
-        /// <see cref="GetLastError" />.
-        /// </para>
-        /// </returns>
-        /// <remarks>
-        /// The results reported by the GetOverlappedResult function are those of the specified handle's last overlapped operation
-        /// to which the specified <see cref="NativeOverlapped" /> structure was provided, and for which the operation's results were
-        /// pending. A pending operation is indicated when the function that started the operation returns FALSE, and the
-        /// GetLastError function returns <see cref="Win32ErrorCode.ERROR_IO_PENDING" />. When an I/O operation is pending, the
-        /// function that started the operation resets the hEvent member of the <see cref="NativeOverlapped" /> structure to the
-        /// nonsignaled state. Then when the pending operation has been completed, the system sets the event object to the signaled
-        /// state.
-        /// <para>
-        /// If the bWait parameter is TRUE, GetOverlappedResult determines whether the pending operation has been completed
-        /// by waiting for the event object to be in the signaled state.
-        /// </para>
-        /// <para>
-        /// If the hEvent member of the <see cref="NativeOverlapped" /> structure is NULL, the system uses the state of the hFile
-        /// handle to signal when the operation has been completed. Use of file, named pipe, or communications-device handles for
-        /// this purpose is discouraged. It is safer to use an event object because of the confusion that can occur when multiple
-        /// simultaneous overlapped operations are performed on the same file, named pipe, or communications device. In this
-        /// situation, there is no way to know which operation caused the object's state to be signaled.
-        /// </para>
-        /// </remarks>
+        /// <inheritdoc cref="GetOverlappedResult(SafeObjectHandle, OVERLAPPED*, out int, bool)"/>
         [NoFriendlyOverloads]
         public static unsafe bool GetOverlappedResult(
             SafeObjectHandle hFile,
