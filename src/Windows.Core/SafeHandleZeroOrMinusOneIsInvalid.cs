101d29ba88fab85025aec3a7a72623b5bc7692b3
Andrew Arnott <andrewarnott@live.com>
2021-02-15 22:03:09 -0700

    Replace all custom code with CsWin32 projections

--- src/Windows.Core/SafeHandleZeroOrMinusOneIsInvalid.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright © .NET Foundation and Contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-// We must type forward so that folks who compiled against our netstandard1.x library
-// can still run when linking against a more recent library at runtime.
-using System.Runtime.CompilerServices;
-using Microsoft.Win32.SafeHandles;
-
-[assembly: TypeForwardedTo(typeof(SafeHandleZeroOrMinusOneIsInvalid))]
